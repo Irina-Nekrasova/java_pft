b58dfcca4722e07121844da43da7989c437a966a
Irina <irina_uni@mail.ru>
2020-03-13 19:35:25 +0300

    Добавлены методы и классы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,23 +4,16 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
    hello("world");
-   double l=8;
-   double a=4;
-   double b=6;
-    System.out.println("Площадь квадарата со стороной " + l + " = " + area(l));
-    System.out.println("Площадь прямоугольника со сторонами " + a  + " и " + b + " = " + area(a,b));
+
+   Square s=new Square(5);
+   Rectangle r=new Rectangle(4,6);
+
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    System.out.println("Площадь прямоугольника со сторонами " + r.a  + " и " + r.b + " = " + r.area());
     }
 
    public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
    }
-
-   public static double area(double l) {
-    return l*l;
-   }
-
-  public static double area(double a,double b) {
-    return a*b;
-  }
 
 }
