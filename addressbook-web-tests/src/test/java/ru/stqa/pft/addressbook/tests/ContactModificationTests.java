535accf0c3dd13f2aca3ab8f5dbfa80da811a286
Irina <irina_uni@mail.ru>
2020-04-07 15:55:54 +0300

    Добавлены отсортированные списки во все тесты по контактам

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -3,6 +3,10 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactModificationTests extends TestBase {
 
@@ -12,13 +16,22 @@ public class ContactModificationTests extends TestBase {
       app.getContactHelper().createContact(new ContactData("Irina", "Nekras", "Sankt-Peter", "89213336677", "true@mail.ru", "test1"), true);
       app.getNavigationHelper().returnToHomePage();
     }
-    int before = app.getContactHelper().getContactCount();
-    app.getContactHelper().initContactModification();
-    app.getContactHelper().fillNewContactForm(new ContactData("Irina", "Nekrasova", "Sankt-Peter", "89213336677", "true@mail.ru", null), false);
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().initContactModification(before.size()-1);
+    ContactData contact = new ContactData(before.get(before.size()-1).getId() , "Arina", "Nekrasova", "Sankt-Peter", "89213336677", "true@mail.ru", null);
+    app.getContactHelper().fillNewContactForm(contact, false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().returnToHomePage();
-    int after = app.getContactHelper().getContactCount();
-    Assert.assertEquals(after, before);
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size()-1);
+    before.add(contact);
+    Comparator<? super ContactData> byID = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byID);
+    after.sort(byID);
+    Assert.assertEquals(before ,after);
+
   }
 
 }
