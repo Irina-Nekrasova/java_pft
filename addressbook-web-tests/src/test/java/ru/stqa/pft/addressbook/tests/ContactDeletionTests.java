16e811c41b7b5b2e581e46b72abc573ac21370de
Irina <irina_uni@mail.ru>
2020-04-07 15:24:53 +0300

    Реализовано сравнение списков при удалении Контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ContactDeletionTests extends TestBase {
@@ -14,12 +15,14 @@ public class ContactDeletionTests extends TestBase {
       app.getContactHelper().createContact(new ContactData("Irina", "Nekras", "Sankt-Peter", "89213336677", "true@mail.ru", "test1"), true);
       app.getNavigationHelper().returnToHomePage();
     }
-    int before = app.getContactHelper().getContactCount();
-    app.getContactHelper().selectContact(before-1);
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContact(before.size()-1);
     app.getContactHelper().deleteSelectedContact();
-    int after = app.getContactHelper().getContactCount();
-    Assert.assertEquals(after, before - 1);
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() - 1);
 
+    before.remove(before.size()-1);
+    Assert.assertEquals(after, before);
   }
 
 }
