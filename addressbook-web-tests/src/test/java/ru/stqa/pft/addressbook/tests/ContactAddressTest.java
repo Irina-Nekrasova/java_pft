2b6670a1368a67261dc342e8e9b0bd9ffc161a11
Irina <irina_uni@mail.ru>
2020-04-14 15:51:05 +0300

    Добавлен тест по проверке адресов

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTest.java
@@ -0,0 +1,33 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactAddressTest extends TestBase {
+
+  @BeforeMethod
+  public void insurePreCondition () {
+    if (app.contact().all().size() == 0) {
+      app.contact().create(new ContactData()
+              .withFirstname("Irina").withLastname("Nekras").withAddress("Sankt-Peter")
+              .withMobile("89213336677").withEmail("true@mail.ru").withGroup("test1"), true);
+      app.goTo().homePage();
+    }
+  }
+
+  @Test
+  public void testAddressInformation() {
+    app.goTo().homePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    assertThat(contact.getAddress(), equalTo(cleaned(contactInfoFromEditForm.getAddress())));
+  }
+
+  public String cleaned(String address) {
+    return address.replaceAll("\\s\\s+", " ");
+  }
+}
