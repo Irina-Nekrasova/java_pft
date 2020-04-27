3f43cfb3c52f99582ef133c9be84c6fa3e8732de
Irina <irina_uni@mail.ru>
2020-04-27 17:14:02 +0300

    Добавлена проверка предусловий для теста по добавления контакта в группу

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddGroup.java
@@ -1,9 +1,9 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
@@ -14,12 +14,27 @@ public class ContactAddGroup extends TestBase {
 
   public GroupData selectedGroup;
 
+  @BeforeMethod
+  public void insurePreCondition() {
+    if (app.db().contacts().size() == 0) {
+      if (app.db().groups().size() == 0) {
+        app.goTo().groupPage();
+        app.group().create(new GroupData().withName("test 11"));
+      }
+      Groups groups = app.db().groups();
+      app.contact().create(new ContactData()
+              .withFirstname("Irina").withLastname("Nekras").withAddress("Sankt-Peter")
+              .withMobile("89213336677").withEmail("true@mail.ru")
+              .inGroup(groups.iterator().next()), true);
+      app.goTo().homePage();
+    }
+  }
+
   @Test
   public void testContactAddGroup() {
-    Contacts contacts = app.db().contacts();
     Groups groups = app.db().groups();
     selectedGroup = groups.iterator().next();
-    ContactData modifiedContact = contacts.iterator().next();
+    ContactData modifiedContact = app.db().contacts().iterator().next();
     Groups allGroupsForContactBefore = modifiedContact.getGroups();
 
 
