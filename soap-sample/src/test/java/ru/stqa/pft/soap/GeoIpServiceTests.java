ccb31a1f41d32ef528ab907e2c6fad0d2fd39dd0
Irina <irina_uni@mail.ru>
2020-05-08 22:56:49 +0300

    Добавлен код по работе с SOAP

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.soap;
+
+import com.lavasoft.GeoIPService;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp() {
+    String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("176.59.1.78");
+    Assert.assertEquals(ipLocation, "RU");
+
+
+  }
+
+}
